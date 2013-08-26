d38a7ecbd0f5307c6a4bc3f0302e6e98f4adc0b7
james <eagle860@163.com>
2013-08-26 23:51:49 +0800

    stm32 test project boot

---
+++ Project/STM32F10x_StdPeriph_Template/main.c
@@ -0,0 +1,58 @@
+#include "stdint.h"
+#include "stdbool.h"
+#include "string.h"
+#include "stm32f10x.h"
+
+
+#define FLASH_START_ADDR    ((uint32_t)0x08000000UL)
+#define FLASH_PAGE_SIZE     ((uint32_t)2048ul)
+#define PAGEn_ADDR(n)       (FLASH_START_ADDR+n*FLASH_PAGE_SIZE)
+
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       file (startup_stm32f10x_xx.s) before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, refer to
+       system_stm32f10x.c file
+     */
+     (*(void (*)())(*(uint32_t *)(PAGEn_ADDR(1)+4)))();
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
